c3e090d82ad0b14926e664a5aecc882dd30070f3
Duong Ngo Hunq <duongngohung1118@gmail.com>
2022-04-16 17:03:48 +0700

    video 11 - hoan tat CRUD

--- src/contronller/homeController.js
+++ src/contronller/homeController.js
@@ -16,14 +16,31 @@ let getDetailPage =async(req, res) =>{
 }
 
 let createNewUser = async(req, res) =>{
-    console.log('>>>Check req: ', req.body);
+    console.log('>>>Check req: ', req);
     let {firstName, lastName, email, address} = req.body;
     await pool.execute('INSERT INTO users (firstName, lastName, email, address) VALUES (?, ?, ?, ?)',
     [firstName, lastName, email, address])
-    
+
     return res.redirect('/')
 }
 
+let deleteUser = async (req, res) =>{
+    let userId = req.body.id;
+    await pool.execute('Delete from users where id = ?', [userId])
+    return res.redirect('/')
+    
+}
+let editUserPage =async (req, res) =>{
+    let id = req.params.id; 
+    let [user] = await pool.execute('select * from users where id = ?', [id])
+    return res.render('update.ejs',{dataUser: user[0]})  
+}
+let postUpdateUser = async(req, res) =>{
+    let {firstName, lastName, email, address, id} = req.body;
+    await pool.execute('update users set firstname = ?, lastName = ?, email = ?, address = ? where id = ?',
+    [firstName, lastName, email, address, id])
+    return res.redirect("/")
+}
 module.exports = {
-    getHomepage, getDetailPage, createNewUser
+    getHomepage, getDetailPage, createNewUser, deleteUser, editUserPage, postUpdateUser
 }
--- src/route/web.js
+++ src/route/web.js
@@ -6,7 +6,10 @@ let router = express.Router();
 const initWebRoute = (app) =>{
     router.get('/', homeController.getHomepage);
     router.get('/detail/user/:id', homeController.getDetailPage)
+    router.get('/edit-user/:id', homeController.editUserPage)
     router.post('/create-new-user', homeController.createNewUser)
+    router.post('/delete-user', homeController.deleteUser)
+    router.post('/update-user',homeController.postUpdateUser)
     router.get('/about', (req, res) =>{
         res.send(`Name: Duong Ngo Hung 
          age: 21
